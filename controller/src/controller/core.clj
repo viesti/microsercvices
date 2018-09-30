d9f7505fc4942d9d7b7f2585fc12f23019f23127
Kimmo Koskinen <kimmo.koskinen@solita.fi>
2018-09-30 21:31:09 +0300

    Add request-count

--- controller/src/controller/core.clj
+++ controller/src/controller/core.clj
@@ -1,11 +1,16 @@
 (ns controller.core
   (:require [org.httpkit.server :as http]))
 
+(def request-count (atom 0))
+
 (defn app [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
-   :body    "hello HTTP!"})
+   :body    (str "Request count: " (swap! request-count inc))})
 
 (defn start []
   (println "starting controller")
   (http/run-server app {:port 8080}))
+
+(defn get-request-count []
+  @request-count)
